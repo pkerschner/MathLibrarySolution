8ca7421c9658b0ce0b5292798f8dfa8520fbe537
Philip Kerschner <pkerschner@zoominternet.net>
2022-02-08 16:02:59 -0500

    Add project files.

---
+++ MathLibraryProject/Math.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathLibraryProject {
+
+    public static class Math {
+
+        public static string Add(string a, string b) {
+            return a + b;
+        }
+        public static decimal Add(decimal a, decimal b) {
+            return a + b;
+        }
+        public static long Add(long a, long b) {
+            return a + b;
+        }
+
+        public static int Add(int a, int b) {
+            return a + b;
+        }
+        public static int Sub(int a, int b) {
+            return a - b;
+        }
+        public static int Mult(int a, int b) {
+            return a * b;
+        }
+        public static int Div(int a, int b) {
+            return a / b;
+        }
+    }
+}
---
+++ MathLibraryProject/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace MathLibraryProject {
+    class Program {
+        static void Main(string[] args) {
+
+            int a = Math.Add(1, 2);
+
+            var aa = Math.Add(1.0m, 2.0m);
+
+            var ab = Math.Add("1", "2");
+        }
+    }
+}
